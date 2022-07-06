d831914b7237333dc5d9ebf9bd7c31d72d3b82b3
MrGunflame <mrgunflame@protonmail.com>
2022-07-06 11:56:37 +0200

    Implement Read for supported types

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,4 @@
-use datastore::{TypeWriter, Write, Writer};
+use datastore::{Read, Reader, TypeWriter, Write, Writer};
 
 use crate::MySqlStore;
 
@@ -194,6 +194,22 @@ impl Write<MySqlStore> for [u8] {
     }
 }
 
+impl Write<MySqlStore> for Vec<u8> {
+    fn write<W>(&self, writer: &mut W) -> Result<(), W::Error>
+    where
+        W: Writer<MySqlStore>,
+    {
+        writer.write_bytes(&self)
+    }
+
+    fn write_type<W>(writer: &mut W) -> Result<(), W::Error>
+    where
+        W: TypeWriter<MySqlStore>,
+    {
+        writer.write_bytes()
+    }
+}
+
 impl Write<MySqlStore> for str {
     fn write<W>(&self, writer: &mut W) -> Result<(), W::Error>
     where
@@ -207,5 +223,140 @@ impl Write<MySqlStore> for str {
         W: TypeWriter<MySqlStore>,
     {
         writer.write_str()
+    }
+}
+
+impl Write<MySqlStore> for String {
+    fn write<W>(&self, writer: &mut W) -> Result<(), W::Error>
+    where
+        W: Writer<MySqlStore>,
+    {
+        writer.write_str(&self)
+    }
+
+    fn write_type<W>(writer: &mut W) -> Result<(), W::Error>
+    where
+        W: TypeWriter<MySqlStore>,
+    {
+        writer.write_str()
+    }
+}
+
+// === impl Read ===
+
+impl Read<MySqlStore> for bool {
+    fn read<R>(reader: &mut R) -> Result<Self, R::Error>
+    where
+        R: Reader<MySqlStore>,
+    {
+        reader.read_bool()
+    }
+}
+
+impl Read<MySqlStore> for i8 {
+    fn read<R>(reader: &mut R) -> Result<Self, R::Error>
+    where
+        R: Reader<MySqlStore>,
+    {
+        reader.read_i8()
+    }
+}
+
+impl Read<MySqlStore> for i16 {
+    fn read<R>(reader: &mut R) -> Result<Self, R::Error>
+    where
+        R: Reader<MySqlStore>,
+    {
+        reader.read_i16()
+    }
+}
+
+impl Read<MySqlStore> for i32 {
+    fn read<R>(reader: &mut R) -> Result<Self, R::Error>
+    where
+        R: Reader<MySqlStore>,
+    {
+        reader.read_i32()
+    }
+}
+
+impl Read<MySqlStore> for i64 {
+    fn read<R>(reader: &mut R) -> Result<Self, R::Error>
+    where
+        R: Reader<MySqlStore>,
+    {
+        reader.read_i64()
+    }
+}
+
+impl Read<MySqlStore> for u8 {
+    fn read<R>(reader: &mut R) -> Result<Self, R::Error>
+    where
+        R: Reader<MySqlStore>,
+    {
+        reader.read_u8()
+    }
+}
+
+impl Read<MySqlStore> for u16 {
+    fn read<R>(reader: &mut R) -> Result<Self, R::Error>
+    where
+        R: Reader<MySqlStore>,
+    {
+        reader.read_u16()
+    }
+}
+
+impl Read<MySqlStore> for u32 {
+    fn read<R>(reader: &mut R) -> Result<Self, R::Error>
+    where
+        R: Reader<MySqlStore>,
+    {
+        reader.read_u32()
+    }
+}
+
+impl Read<MySqlStore> for u64 {
+    fn read<R>(reader: &mut R) -> Result<Self, R::Error>
+    where
+        R: Reader<MySqlStore>,
+    {
+        reader.read_u64()
+    }
+}
+
+impl Read<MySqlStore> for f32 {
+    fn read<R>(reader: &mut R) -> Result<Self, R::Error>
+    where
+        R: Reader<MySqlStore>,
+    {
+        reader.read_f32()
+    }
+}
+
+impl Read<MySqlStore> for f64 {
+    fn read<R>(reader: &mut R) -> Result<Self, R::Error>
+    where
+        R: Reader<MySqlStore>,
+    {
+        reader.read_f64()
+    }
+}
+
+impl Read<MySqlStore> for Vec<u8> {
+    fn read<R>(reader: &mut R) -> Result<Self, R::Error>
+    where
+        R: Reader<MySqlStore>,
+    {
+        reader.read_byte_buf()
+    }
+}
+
+impl Read<MySqlStore> for String {
+    fn read<R>(reader: &mut R) -> Result<Self, R::Error>
+    where
+        R: Reader<MySqlStore>,
+    {
+        reader.read_string()
     }
 }
